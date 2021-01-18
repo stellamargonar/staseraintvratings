6cffd280ca971b2e47f11d8d9e06d45abcb99c10
Stella Margonar <margonar@spaziodati.eu>
2021-01-18 08:30:46 +0100

    check if none

--- telebot/db.py
+++ telebot/db.py
@@ -94,10 +94,11 @@ class DBHelper:
         row = c.fetchone()
         req_at_hour = {}
         total = 0
-        for i, val in enumerate(row):
-            if val is not None and val > 0:
-                req_at_hour[i] = val
-                total += val
+        if row:
+            for i, val in enumerate(row):
+                if val is not None and val > 0:
+                    req_at_hour[i] = val
+                    total += val
         text = f"<b>Richieste di oggi: {total}</b>\n  "
         if len(req_at_hour):
             text += "\n  ".join(f"🕐 {hour}: {n_req}" for hour, n_req in req_at_hour.items())
@@ -106,10 +107,11 @@ class DBHelper:
         row = c.fetchone()
         req_at_hour = {}
         total = 0
-        for i, val in enumerate(row):
-            if val is not None and val > 0:
-                req_at_hour[i] = val
-                total += val
+        if row:
+            for i, val in enumerate(row):
+                if val is not None and val > 0:
+                    req_at_hour[i] = val
+                    total += val
         text += f"\n\n<b>Richieste totali: {total}</b>\n  "
         if len(req_at_hour):
             text += "\n  ".join(f"🕐 {hour}: {n_req}" for hour, n_req in req_at_hour.items())
